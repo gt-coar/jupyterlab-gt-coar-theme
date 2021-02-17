1e0461823145119627bca2bb531d8df6c92d7efe
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2021-02-17 15:45:10 -0500

    use typographic values for switches (#6)

--- src/tokens.ts
+++ src/tokens.ts
@@ -3,11 +3,13 @@
 | Distributed under the terms of the BSD-3-Clause License.
 */
 
-import GT_SVG from '../style/img/wordmark.svg';
+import WORDMARK_SVG from '../style/img/wordmark.svg';
+import WORDMARK_URL from '!!file-loader?name=[path][name].[ext]&context=.!../style/img/wordmark.svg';
+import CHEVRONS_URL from '!!file-loader?name=[path][name].[ext]&context=.!../style/img/chevrons.svg';
 
 export const NS = '@gt-coar/jupyterlab-theme';
 export const NAME = 'GT COAR';
 
-export const GT_ICON_ID = `${NS}:wordmark`;
+export const WORDMARK_ICON_ID = `${NS}:wordmark`;
 
-export { GT_SVG };
+export { WORDMARK_URL, CHEVRONS_URL, WORDMARK_SVG };
