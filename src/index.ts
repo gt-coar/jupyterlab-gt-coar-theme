0caa473656bec1067e92aa4b5ed26e92cd25fa92
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2021-02-17 15:28:56 -0500

    faviscon

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,23 @@
 
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
 import { IThemeManager } from '@jupyterlab/apputils';
+import { PageConfig } from '@jupyterlab/coreutils';
 import { LabIcon, jupyterFaviconIcon } from '@jupyterlab/ui-components';
 
-import { NS, NAME, GT_SVG, GT_ICON_ID } from './tokens';
+import {
+  NS,
+  NAME,
+  WORDMARK_SVG,
+  WORDMARK_ICON_ID,
+  CHEVRONS_URL,
+  WORDMARK_URL,
+} from './tokens';
 
-export const GT_ICON = new LabIcon({ name: GT_ICON_ID, svgstr: GT_SVG });
+export const GT_ICON = new LabIcon({ name: WORDMARK_ICON_ID, svgstr: WORDMARK_SVG });
 
 const OG_FAVICON = jupyterFaviconIcon.svgstr;
+const OG_FAVICON_MIME = 'image/x-icon';
+const GT_FAVICON_MIME = 'image/svg+xml';
 
 function makeTheme(value: string): JupyterFrontEndPlugin<void> {
   return {
@@ -21,20 +31,46 @@ function makeTheme(value: string): JupyterFrontEndPlugin<void> {
     activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
       let wasLoaded = false;
       const isLight = value == 'Light';
+      let faviconIdle: HTMLLinkElement;
+      let faviconBusy: HTMLLinkElement;
+
+      let ogFaviconIdle: string;
+      let ogFaviconBusy: string;
+      const baseUrl = PageConfig.getBaseUrl();
+
+      function toggleFavicons(restore = false) {
+        if (faviconIdle == null) {
+          faviconIdle = document.querySelector('link.favicon.idle');
+          ogFaviconIdle = `${baseUrl}static/favicons/favicon.ico`;
+          faviconBusy = document.querySelector('link.favicon.busy');
+          ogFaviconBusy = `${baseUrl}static/favicons/favicon-busy-1.ico`;
+        }
+        if (restore) {
+          faviconIdle.href = ogFaviconIdle;
+          faviconBusy.href = ogFaviconBusy;
+          faviconIdle.type = faviconBusy.type = OG_FAVICON_MIME;
+          jupyterFaviconIcon.svgstr = OG_FAVICON;
+        } else {
+          faviconIdle.href = WORDMARK_URL;
+          faviconBusy.href = CHEVRONS_URL;
+          faviconIdle.type = faviconBusy.type = GT_FAVICON_MIME;
+          jupyterFaviconIcon.svgstr = WORDMARK_SVG;
+        }
+      }
 
       manager.register({
         name: `${NAME} ${value}`,
         isLight,
         themeScrollbars: !isLight,
         load: async () => {
-          jupyterFaviconIcon.svgstr = GT_SVG;
+          toggleFavicons();
           // avoid loading twice
           wasLoaded ? void 0 : manager.loadCSS(`${NS}/index.css`);
           wasLoaded = true;
         },
         unload: async () => {
-          if (jupyterFaviconIcon.svgstr === GT_SVG) {
-            jupyterFaviconIcon.svgstr = OG_FAVICON;
+          if (jupyterFaviconIcon.svgstr === WORDMARK_SVG) {
+            toggleFavicons(true);
           }
         },
       });
